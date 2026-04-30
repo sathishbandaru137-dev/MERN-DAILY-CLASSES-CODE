da8ee26eee221dba7bd223d09df324b97a785bd3
sathishbandaru137-dev <sathishbandaru137@gmail.com>
2026-05-01 00:09:09 +0530

    day7 and day8

---
+++ javascript/external.js
@@ -0,0 +1,45 @@
+num="java"
+num="50"
+console.log(num);
+
+num=true
+console.log(num);
+
+class sampleExample{
+    display(){
+        console.log("this is non-static methood in sample example class");
+    }
+    
+Test(){
+    console.log("test method");
+}
+
+}
+ s1= new sampleExample();
+ s1.display();
+ s1.Test();
+
+ function addition(){
+    console.log(10+20);
+ }
+ addition ();
+
+ num=332;
+
+ result=num%2==0?"even number":"odd number";
+ console.log(result);
+
+ a=10;
+ b='20';
+ c=null;
+
+ e=154254849867636n
+ let f;
+
+ console.log("type of varialble is:",typeof a);
+ console.log("type of varialble is:",typeof b);
+ console.log("type of varialble is:",typeof c);
+ console.log("type of varialble is:",typeof d);
+ console.log("type of varialble is:",typeof e);
+console.log("type of varialble is:",typeof f);
+
