fdf83f4da34a709d9c83c914b9c693a542ea6fd2
sathishbandaru137-dev <sathishbandaru137@gmail.com>
2026-05-09 09:42:45 +0530

    day-16

---
+++ sample-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className='bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDJCWB2yamEmn8cqWk4yP8aFbYsu3m-_IFpA&s)] h-200 w-350'>
+      <h1 className='text-center font-bold bg-amber-200 m-3  p-5 rounded-4xl shadow-2xl shadow-amber-400'> sample project</h1>
+      <h2 className="backdrop-blur-2xl w-50 text-center text-2xl font-bold shadow-2xl shadow-amber-500 rounded-3xl text-white">
+    welcome TO TAILWINDCSS</h2>
+
+    <div>
+   <div className="grid-cols-4 gap-x-2 gap-y-2 grid bg-blue-200 rounded-2xl p-10" >
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  <div className="bg-red-400 h-10 w-30 rounded-2xl shadow-2xl shadow-black" ></div>
+  </div>
+
+</div>
+  </div>
+  )
+}
+
+export default App
